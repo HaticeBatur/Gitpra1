ce9c9f3a97d767b71d02dfabbc1f1db46542c626
Hatice Batur <hb.baturhatice@gmail.com>
2020-02-05 15:40:51 -0600

    add change

--- src/Test/Home.java
+++ src/Test/Home.java
@@ -4,5 +4,7 @@ public class Home {
 	public static void main(String[] args) {
 		System.out.println("This is git practice");
 		System.out.println("Add sample");
+		System.out.println("Sample add");
+
 	}
 }
